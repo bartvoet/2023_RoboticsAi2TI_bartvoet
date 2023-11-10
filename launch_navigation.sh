f2557cea3114e957a74b382b46ff3c7878853ebf
Bart Voet <bart_voet@telenet.be>
2023-11-10 11:24:49 +0100

    Mogelijkheid om ROS-id toe te voegen voor navigatie

--- launch_navigation.sh
+++ launch_navigation.sh
@@ -1,2 +1,7 @@
-ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/mapGazeboTurtlebot.yaml
 
+if [ "$#" -eq 1 ]; then
+    echo "Setting ROS_DOMAIN_ID to $1"
+    export ROS_DOMAIN_ID=$1
+fi
+
+ros2 launch turtlebot3_navigation2 navigation2.launch.py use_sim_time:=True map:=$HOME/mapGazeboTurtlebot${ROS_DOMAIN_ID}.yaml
--- save_map.sh
+++ save_map.sh
@@ -1,1 +1,5 @@
-ros2 run nav2_map_server map_saver_cli -f ~/mapGazeboTurtlebot
+if [ "$#" -eq 1 ]; then
+    echo "Setting ROS_DOMAIN_ID to $1"
+    export ROS_DOMAIN_ID=$1
+fi
+ros2 run nav2_map_server map_saver_cli -f ~/mapGazeboTurtlebot${ROS_DOMAIN_ID}
