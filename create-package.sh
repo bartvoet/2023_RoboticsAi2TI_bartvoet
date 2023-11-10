dfe1861a21e1193160d49849d7b6bdb0f4fee577
Bart Voet <bart_voet@telenet.be>
2023-11-10 13:43:09 +0100

    Rekening houden met camelcase bij generatie package

--- create-package.sh
+++ create-package.sh
@@ -8,4 +8,4 @@ mkdir ${name}_pkg/launch
 sed "s/xxx/${name}/g" ./template_launch > ${launchfile}
 chmod u+x ${launchfile}
 sed "s/xxx/${name}/g" ./template_setup > ${packagename}/setup.py
-sed "s/xxx/${name}/g" ./template_script > ${packagename}/${packagename}/${name}.py
+sed -e "s/xxx/${name}/g" -e "s/yyy/${name^}/g" ./template_script > ${packagename}/${packagename}/${name}.py
