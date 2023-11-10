1824e066ee510a3a6adabc5614af0bd5ae67193c
Bart Voet <bart_voet@telenet.be>
2023-11-10 10:56:27 +0100

    Toevoegen van patrol

--- src/patrol_pkg/patrol_pkg/patrol.py
+++ src/patrol_pkg/patrol_pkg/patrol.py
@@ -7,14 +7,52 @@ from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 from rclpy.qos import ReliabilityPolicy, QoSProfile
 
-class Patrol(Node):
+class  Patrol(Node):
 
     def __init__(self):
         # Here you have the class constructor
         # call the class constructor
         super().__init__('patrol')
+        # create the publisher object
+        self.publisher_ = self.create_publisher(Twist, 'cmd_vel', 10)
+        # create the subscriber object
+        self.subscriber = self.create_subscription(LaserScan, '/scan', self.laser_callback, QoSProfile(depth=10, reliability=ReliabilityPolicy.RELIABLE))
+        # define the timer period for 0.5 seconds
+        self.timer_period = 0.5
+        # define the variable to save the received info
+        self.laser_forward = 0
+        self.laser_frontLeft = 0
+        self.laser_frontRight = 0
 
-           
+        # create a Twist message
+        self.cmd = Twist()
+        self.timer = self.create_timer(self.timer_period, self.motion)
+
+    def laser_callback(self,msg):
+        # Save the frontal laser scan info at 0°
+        self.laser_forward = msg.ranges[359]
+        self.laser_frontLeft = min(msg.ranges[0:15])
+        self.laser_frontRight = min(msg.ranges[345:359])
+
+
+        
+    def motion(self):
+        # print the data
+        self.get_logger().info('Forward: "%s"' % str(self.laser_forward))
+        
+        if (self.laser_frontLeft < 0.5) :
+            self.get_logger().info('Object front left: "%s"' % str(self.laser_frontLeft))
+        if (self.laser_frontRight < 0.5) :
+            self.get_logger().info('Object front right: "%s"' % str(self.laser_frontRight))
+
+        # Logic of move
+        self.cmd.linear.x = 0.0
+        self.cmd.angular.z = 0.0
+        # Publishing the cmd_vel values to a Topic
+        # self.publisher_.publish(self.cmd)
+
+
+            
 def main(args=None):
     # initialize the ROS communication
     rclpy.init(args=args)
--- src/patrol_pkg/setup.py
+++ src/patrol_pkg/setup.py
@@ -23,7 +23,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'subpub = patrol_pkg.patrol:main'
+            'patrol = patrol_pkg.patrol:main'
         ],
     },
 )
