b3c6b8e9b5a533b74336d2b5b1a6de85e2d4478f
Bart Voet <bart_voet@telenet.be>
2023-11-23 23:35:07 +0100

    Isolatie naar apart lidar-object

--- src/patrol_pkg/patrol_pkg/patrol.py
+++ src/patrol_pkg/patrol_pkg/patrol.py
@@ -25,14 +25,7 @@ class  Patrol(Node):
                                                    QoSProfile(depth=10, reliability=ReliabilityPolicy.SYSTEM_DEFAULT))
         # define the timer period for 0.5 seconds
         self.timer_period = 0.50
-        # define the variable to save the received info
-        self.laser_forward = 0
-        self.laser_frontLeft = 0
-        self.laser_frontRight = 0
         self.speed = speed
-
-        self.leftDistance = 0
-        self.rightDistance = 0
 
         # create a Twist message
         self.cmd = Twist()
@@ -40,38 +33,9 @@ class  Patrol(Node):
         self.lastMsg = None
 
     def laser_callback(self,msg):
-        boundary=len(msg.ranges) // 12
-        # Save the frontal laser scan info at 0°
-        self.laser_forward = msg.ranges[-1]
-        self.laser_frontLeft = min(msg.ranges[0:boundary])
-        self.laser_frontRight = min(msg.ranges[-boundary:])
-        self.leftDistance = msg.ranges[len(msg.ranges) // 4]
-        self.rightDistance = msg.ranges[len(msg.ranges) // 4 * 3]
-        self.backDistance = msg.ranges[len(msg.ranges) // 4 * 3]
-        self.lastMsg = msg.ranges
-
-    def rangeFromCenter(self, degrees):
-        if self.lastMsg:
-            range = (len(self.lastMsg) * (degrees) // 360) // 2
-            self.log(f"calc range: {len(self.lastMsg)} {range}")
-            return self.lastMsg[-range:] + self.lastMsg[0:range]
-        return None
-
-
-    def minRangeFromCenter(self, degrees):
-        range = self.rangeFromCenter(degrees)
-        if range:
-            return min(range)
-        return None
-
-    def avgRangeFromCenter(self, degrees):
-        range = self.rangeFromCenter(degrees)
-        if range:
-            return sum(range) / len(range)
-        return None
+        self.lidar = Lidar(msg,self.get_logger())
 
     def calculateAngularRequiredFor(self, degrees):
-        #return 1.0
         return radians(degrees) * (1 / self.timer_period)
 
     def log(self, msg):
@@ -98,13 +62,6 @@ class  Patrol(Node):
     def go(self):
         self.publisher_.publish(self.cmd)
 
-    def logDistances(self):
-        self.log(f"Forward: {self.laser_forward}")
-        self.log(f"Front-left {self.laser_frontLeft}")
-        self.log(f"Front-right {self.laser_frontRight}")
-        self.log(f"left {self.leftDistance}")
-        self.log(f"right {self.rightDistance}")
-
     def stop(self):
         self.cmd.linear.x = 0.0
         self.cmd.angular.z = 0.0
@@ -112,19 +69,20 @@ class  Patrol(Node):
     def motion(self):
         # Logic of move
 
-        self.logDistances()
+        if self.lidar is None:
+            return
+
+        self.lidar.logDistances()
         #forward = self.laser_forward
-        forward = self.minRangeFromCenter(5)
+        forward = self.lidar.minRangeFromCenter(5)
         self.log(f"new forward: {forward}" )
 
         for i in range(1,12):
-            self.log(f"avg for {i * 10} ->  {self.avgRangeFromCenter(i * 10)}" )
+            self.log(f"avg for {i * 10} ->  {self.lidar.avgRangeFromCenter(i * 10)}" )
 
         if forward is None:
             self.stop()
             return
-
-        self.log("aaaa")
 
         if forward > 5:
             self.goForward(self.speed)
@@ -133,13 +91,13 @@ class  Patrol(Node):
         elif forward >= 0.3:
             self.goForward(self.speed / 4)
         else:
-            if math.isnan(self.leftDistance):
+            if math.isnan(self.lidar.leftDistance):
                 self.turnRight(5)
-            elif math.isnan(self.rightDistance):
+            elif math.isnan(self.lidar.rightDistance):
                 self.turnLeft(5)
 
             #if self.leftDistance > self.rightDistance:
-            if self.laser_frontLeft < self.laser_frontRight:
+            if self.lidar.laser_frontLeft < self.lidar.laser_frontRight:
                 self.turnRight(40)
             else:
                 self.turnLeft(40)
@@ -157,6 +115,49 @@ def main(args=None):
     patrol.destroy_node()
     # shutdown the ROS communication
     rclpy.shutdown()
+
+class Lidar:
+    def __init__(self, msg, logger):
+        self.logger = logger
+        if msg:
+            ranges = msg.ranges
+            boundary=len(ranges) // 12
+            self.laser_forward = ranges[-1]
+            self.laser_frontLeft = min(ranges[0:boundary])
+            self.laser_frontRight = min(ranges[-boundary:])
+            self.leftDistance = ranges[len(ranges) // 4]
+            self.rightDistance = ranges[len(ranges) // 4 * 3]
+            self.backDistance = ranges[len(ranges) // 4 * 3]
+            self.lastMsg = ranges
+
+    def log(self, msg):
+        self.logger.info(str(msg))
+
+    def logDistances(self):
+        self.log(f"Forward: {self.laser_forward}")
+        self.log(f"Front-left {self.laser_frontLeft}")
+        self.log(f"Front-right {self.laser_frontRight}")
+        self.log(f"left {self.leftDistance}")
+        self.log(f"right {self.rightDistance}")
+
+    def rangeFromCenter(self, degrees):
+        if self.lastMsg:
+            range = (len(self.lastMsg) * (degrees) // 360) // 2
+            self.log(f"calc range: {len(self.lastMsg)} {range}")
+            return self.lastMsg[-range:] + self.lastMsg[0:range]
+        return None
+
+    def minRangeFromCenter(self, degrees):
+        range = self.rangeFromCenter(degrees)
+        if range:
+            return min(range)
+        return None
+
+    def avgRangeFromCenter(self, degrees):
+        range = self.rangeFromCenter(degrees)
+        if range:
+            return sum(range) / len(range)
+        return None
 
 if __name__ == '__main__':
     main()
